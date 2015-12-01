d1627b67a9fb98aa78d55b313a6faa85ad52577c
Louis Cheminant <louischeminant@me.com>
2015-12-01 08:22:59 +0100

    Ajout : Ajout de la multioffre

--- jsonentreprise.php
+++ jsonentreprise.php
@@ -2,29 +2,39 @@
  
 header('Content-type: application/json');
 
-$db_name     = 'myjobs';
-$db_user     = 'louischeminant';
-$db_password = 'OustamineL040194';
-$server_url  = '127.0.0.1';
+if($_POST) {
+	$compagny 		= $_POST['compagny'];
 
-$mysqli = new mysqli($server_url, $db_user, $db_password, $db_name);
+	$db_name     = 'myjobs';
+	$db_user     = 'louischeminant';
+	$db_password = 'OustamineL040194';
+	$server_url  = '127.0.0.1';
 
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} else {
-	$sql = "SELECT announcements.compagny, type, offer, missions, level, announcements.address, latitude, longitude, compagnies.description, announcements.id  FROM announcements INNER JOIN compagnies ON compagnies.name=announcements.compagny";
-	if ($result = mysqli_query($mysqli, $sql)) {
-		$resultArray = array();
-		$tempArray = array();
+	$mysqli = new mysqli($server_url, $db_user, $db_password, $db_name);
 
-		while($row = $result->fetch_object()) {
-			$tempArray = $row;
-	    	array_push($resultArray, $tempArray);
+	if (mysqli_connect_errno())
+	{
+  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	} else {
+		if ($stmt = $mysqli->prepare("SELECT announcements.compagny, type, offer, missions, level, announcements.address, latitude, longitude, compagnies.description, announcements.id  FROM announcements INNER JOIN compagnies ON compagnies.name=announcements.compagny WHERE announcements.compagny = ?")) {
+			$stmt->bind_param("s", $compagny);
+			
+			$resultArray = array();
+			$tempArray = array();
+
+			$stmt->execute();
+			$result = $stmt->get_result();
+			
+			while ($row = $result->fetch_object()) {
+					$tempArray = $row;
+	    			array_push($resultArray, $tempArray);
+				}
+			echo json_encode($resultArray);
+			$stmt->free_result();
+			$stmt->close();
 		}
-		echo json_encode($resultArray);
+		mysqli_close($mysqli);
 	}
-	mysqli_close($mysqli);
 }
 
 ?>
